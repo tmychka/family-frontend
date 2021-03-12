187ed0bd331a5c03c392ece406786d189f4748cd
tmychka <timmychka@gmail.com>
2021-03-12 16:06:55 +0200

    поправляв

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import './index.css'
 import App from './components/App/App';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDOM.render (
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
